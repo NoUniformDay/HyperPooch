7ecaf0412ed35d861e52f7748ad615d411d0da88
Eric McEvoy <eric.mcevoy95@gmail.com>
2018-03-13 20:44:40 +0000

    edited startup script

--- scripts/startUp.sh
+++ scripts/startUp.sh
@@ -45,19 +45,4 @@ echo
 echo
 
 
-echo "POST instantiate chaincode on peer1 of Org1"
-echo
-curl -s -X POST \
-  http://localhost:4000/channels/mychannel/chaincodes \
-  -H "content-type: application/json" \
-  -d '{
-  	"orgName" : "org1",
-  	"username" : "eric",
-  	"channelName" : "mychannel",
-	"chaincodeName":"mycc",
-	"chaincodeVersion":"v0",
-	"args":["a","100","b","200"]
-}'
-echo
-echo
 
